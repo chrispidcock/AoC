9115658007aa81887d2cb3bb3145c463f88b3a28
chrispidcock <chris.pidcock87@gmail.com>
2021-05-10 05:20:04 +1000

    day12 WIP

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -19,8 +20,50 @@ func main() {
 	fmt.Println("logsActive ", logsActive)
 	defer timeTrack(time.Now(), "day12")
 	input, _ := readLines("nav-i-part-a-test.txt")
-	// input, _ := readLines("seat-test-part-b.txt")
+	// input, _ := readLines("nav-instructions.txt")
 
+	var dir []string
+	var dist []int
+	for i := range input {
+		dir = append(dir, string(input[i][0]))
+		distance, _ := strconv.Atoi(input[i][1:])
+		dist = append(dist, distance)
+	}
+
+	north := 0
+	east := 0
+	facing := "E"
+	manhattan_dist := Abs(north) + Abs(east)
+
+	fmt.Println("manhattan_dist ", manhattan_dist)
+}
+
+func compass(facing string, degrees int) (new_facing string) {
+	compass := []string{"N", "E", "S", "W"}
+	var compass_index int
+	for i := range compass {
+		if compass[i] == facing {
+			compass_index = i
+			break
+		}
+	}
+	_, change := divMod(degrees/90, 360)
+	_, new_index := divMod(compass_index+change, 4)
+	return compass[new_index]
+}
+
+// Abs returns the absolute value of x.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func divMod(numerator, denominator int) (quotient, remainder int) {
+	quotient = numerator / denominator // integer division, decimals are truncated
+	remainder = numerator % denominator
+	return
 }
 
 func timeTrack(start time.Time, name string) {
