718dea359ab9bbd3ed06d2ae0b0adcdaa6648ba8
chrispidcock <chris.pidcock87@gmail.com>
2021-06-04 08:44:34 +1000

    day25 part1 done

--- 2020/day25/main.go
+++ 2020/day25/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -28,7 +29,84 @@ func main() {
 	// part := "B"
 
 	defer timeTrack(time.Now(), "day25")
-	// input, _ := readLines("public-keys.txt")
+	input, _ := readLines("public-keys.txt")
+	// input, _ := readLines("public-keys-test.txt")
+
+	var public_keys []int
+
+	for i := range input {
+		v, err := strconv.Atoi(input[i])
+		if err != nil {
+			panic("public key is not an integer")
+		}
+		public_keys = append(public_keys, v)
+	}
+
+	fmt.Println("public_keys=", public_keys)
+	// var c_d [][]int
+	var loops []int
+
+	for pk := range public_keys {
+		fmt.Println("pk=", pk)
+		// c_d = append(c_d, make([]int, 0))
+		// c_d[pk] = append(c_d[pk], public_keys[len(public_keys)-1-pk])
+		done := false
+		loop_size := 0
+		sub_num := 7
+		val := 1
+
+		for !done {
+			val = loop(val, sub_num)
+			loop_size = loop_size + 1
+			if val == public_keys[pk] {
+				loops = append(loops, loop_size)
+				done = true
+			}
+		}
+	}
+	fmt.Println("loops=", loops)
+
+	var encr_key int
+	for pk := range public_keys {
+		fmt.Println("pk=", pk)
+		loop_size := loops[pk]
+		sub_num := public_keys[len(public_keys)-1-pk]
+		val := 1
+
+		for i := 0; i < loop_size; i++ {
+			val = loop(val, sub_num)
+		}
+		encr_key = val
+	}
+	fmt.Println("loops=", loops)
+
+	// done := false
+	// var encr_key int
+	// for !done {
+	// 	for pk := range public_keys {
+	// 		sub_num := c_d[pk][len(c_d[pk])-1]
+	// 		val := loops[pk]
+
+	// 		val = loop(val, sub_num)
+	// 		c_d[pk] = append(c_d[pk], val)
+	// 		// fmt.Println(c_d)
+	// 		for i := range c_d[len(public_keys)-1-pk] {
+	// 			if val == c_d[len(public_keys)-1-pk][i] {
+	// 				encr_key = val
+	// 				done = true
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	fmt.Println("\n--- Answer Part 1 ---")
+	fmt.Println("encr_key =", encr_key)
+}
+
+func loop(i int, sub int) int {
+	i = sub * i
+	_, i = divMod(i, 20201227)
+	return i
 }
 
 func getInt(s string) (int, error) {
@@ -48,28 +126,6 @@ func getInt(s string) (int, error) {
 		}
 	}
 	return v, errors.New("No Integers")
-}
-
-func threeCups(s []int, idx int) ([]int, []int, int) {
-	var t [3]int
-	var i = idx
-
-	for idx {
-
-	}
-
-	return s, t, idx
-}
-
-func destCup(s []int, idx int) ([]int, []int) {
-	var t [3]int
-	var i = idx
-
-	for i < len(s) {
-
-	}
-
-	return s, t
 }
 
 // removeIndex. Swap the element to delete with the one at the end of the slice and then return the n-1 first elements
